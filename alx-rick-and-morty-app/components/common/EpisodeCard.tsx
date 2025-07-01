ab7372f60f3e954d54f0a0c581811904c118511c
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-07-01 11:08:33 +0300

    initial commit

---
+++ alx-rick-and-morty-app/components/common/EpisodeCard.tsx
@@ -0,0 +1,25 @@
+import { EpisodeCardProps } from "@/interfaces";
+
+const EpisodeCard: React.FC<EpisodeCardProps> = ({
+  id,
+  air_date,
+  episode,
+  name,
+}) => {
+  return (
+    <div
+      key={id}
+      className="bg-white cursor-pointer shadow-md rounded-lg p-4 m-4 transition-transform duration-200 hover:scale-105"
+    >
+      <div className="flex justify-between items-center">
+        <h2 className="text-xl font-semibold text-gray-800">{name}</h2>
+        <span className="border px-2 text-xs rounded-full bg-blue-600 text-white flex items-center">
+          {episode}
+        </span>
+      </div>
+      <p className="text-gray-600">{air_date}</p>
+    </div>
+  );
+};
+
+export default EpisodeCard;
